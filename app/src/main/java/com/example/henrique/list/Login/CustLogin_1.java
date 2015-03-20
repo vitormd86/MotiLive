c28ffac62f632b29f76d83ea0e475c8183e84a07
htamashiro <henriquexx@gmail.com>
2015-03-20 17:35:26 -0300

    Login + ws

--- app/src/main/java/com/example/henrique/list/Login/CustLogin_1.java
+++ app/src/main/java/com/example/henrique/list/Login/CustLogin_1.java
@@ -1,29 +1,60 @@
 package com.example.henrique.list.Login;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.henrique.list.Cliente.CustDrawerMenu_10;
 import com.example.henrique.list.R;
+import com.example.henrique.list.Utilidade_Publica.Utility;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by htamashiro on 3/16/15.
  */
-public class CustLogin_1 extends FragmentActivity {
+public class CustLogin_1 extends Activity {
+
+
+    ProgressDialog prgDialog;
+    TextView errorMsg;
+    EditText emailET;
+    EditText pwdET;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custlogin_layout_1);
 
+
+        errorMsg = (TextView)findViewById(R.id.login_error);
+        // Find Email Edit View control by ID
+        emailET = (EditText)findViewById(R.id.custLoginNome);
+        // Find Password Edit View control by ID
+        pwdET = (EditText)findViewById(R.id.custLoginSenha);
+        // Instantiate Progress Dialog object
+        prgDialog = new ProgressDialog(this);
+        // Set Progress Dialog Text
+        prgDialog.setMessage("Please wait...");
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
+
+
+
+
         Button criarConta = (Button) findViewById(R.id.buttonCustLoginCriarConta);
-        EditText nome = (EditText) findViewById(R.id.custLoginNome );
-        EditText senha = (EditText) findViewById(R.id.custLoginSenha );
         Button logar = (Button) findViewById(R.id.buttonCustLoginLogar);
 
         logar.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +84,119 @@ public class CustLogin_1 extends FragmentActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    /**
+     * Method gets triggered when Login button is clicked
+     *
+     * @param view
+     */
+    public void  loginUser(View view){
+
+        // Get Email Edit View Value
+        String email = emailET.getText().toString();
+        // Get Password Edit View Value
+        String password = pwdET.getText().toString();
+        // Instantiate Http Request Param Object
+        RequestParams params = new RequestParams();
+        // When Email Edit View and Password Edit View have values other than Null
+        if(Utility.isNotNull(email) && Utility.isNotNull(password)){
+            // When Email entered is Valid
+            if(Utility.validate(email)){
+                // Put Http parameter username with value of Email Edit View control
+                params.put("username", email);
+                // Put Http parameter password with value of Password Edit Value control
+                params.put("password", password);
+                // Invoke RESTful Web Service with Http parameters
+                invokeWS(params);
+            }
+            // When Email is invalid
+            else{
+                Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_LONG).show();
+            }
+        } else{
+            Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
+        }
+
+
+
+}
+
+
+    /*
+          * Method that performs RESTful webservice invocations
+          *
+          * @param params
+          */
+    public void invokeWS(RequestParams params){
+        // Show Progress Dialog
+        prgDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://192.168.2.2:9999/useraccount/login/dologin",params ,new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getBoolean("status")){
+                        Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
+                        // Navigate to Home screen
+                        navigatetoHomeActivity();
+                    }
+                    // Else display error message
+                    else{
+                        errorMsg.setText(obj.getString("error_msg"));
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public void navigatetoHomeActivity(){
+        Intent homeIntent = new Intent(getApplicationContext(),CustProfileChoose_4.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+    }
+    public void navigatetoRegisterActivity(View view){
+        Intent loginIntent = new Intent(getApplicationContext(),CustLoginNewAccount_2.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(loginIntent);
+    }
+    /**
+     * Set degault values for Edit View controls
+     */
+    public void setDefaultValues(){
+        emailET.setText("");
+        pwdET.setText("");
     }
 
 
