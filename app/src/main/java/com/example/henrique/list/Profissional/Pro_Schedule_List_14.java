005439b02c785edf6f6015f52c61fb759bcd1712
htamashiro <henriquexx@gmail.com>
2015-03-21 19:18:22 -0300

    Arrumando a casa

---
+++ app/src/main/java/com/example/henrique/list/Profissional/Pro_Schedule_List_14.java
@@ -0,0 +1,20 @@
+package com.example.henrique.list.Profissional;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.henrique.list.R;
+
+/**
+ * Created by htamashiro on 3/21/15.
+ */
+public class Pro_Schedule_List_14 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.pro_schedule_list_14, parent, false);
+        return v;
+
+    }
+}
