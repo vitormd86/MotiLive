02593fdbf8e3911f8ca6dcd92df844f98cc16d9d
michael <msantaguida@gmail.com>
2015-05-01 19:38:16 -0300

    Adiçao dos novos serviços de buscar os clientes que são contatos do profissional, e buscar os profissionais que são contatos do cliente

---
+++ app/src/main/java/com/example/henrique/list/Profissional/ProServiceNewActivity_6.java
@@ -0,0 +1,21 @@
+package com.example.henrique.list.Profissional;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.example.henrique.list.R;
+
+/**
+ * Created by michael on 01/05/2015.
+ */
+public class ProServiceNewActivity_6 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pro_service_new_6);
+    }
+
+
+}
