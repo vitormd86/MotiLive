d8291e9929a33001301edf25060821f6dc049a9d
Vitor <vitormd86@gmail.com>
2015-05-18 23:06:47 -0300

    Tela Login_2 com intents e conferencia de senha funcionais. Layout arrumado.

--- app/src/main/java/com/example/henrique/list/Login/LoginNewAccount_2.java
+++ app/src/main/java/com/example/henrique/list/Login/LoginNewAccount_2.java
@@ -1,162 +1,71 @@
 package com.example.henrique.list.Login;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.henrique.list.R;
-
-
-
 /**
  * Created by htamashiro on 3/17/15.
  *
- * //TODO: definir se vai existir um cadastro completo ou somente esse.
+ *
  */
 public class LoginNewAccount_2 extends Activity {
 
-
-    // Progress Dialog Object
-    ProgressDialog prgDialog;
-    // Error Msg TextView Object
-    TextView errorMsg;
-    // Name Edit View Object
-    EditText nameET;
-    // Email Edit View Object
-    EditText emailET;
-    // Passwprd Edit View Object
-    EditText pwdET;
-
+    EditText nameET, passwordET, confirmPasswordET;
+    Button createAccountBT, cancelBT;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_new_account_2);
 
+        initViews();
+        setCreateAccountButtonListener();
+        setCancelButtonListener();
+        //todo criar funcionamento do checkbox "mostrar senha"
+    }
+
+    private void initViews(){
         nameET = (EditText) findViewById(R.id.loginNewAccountName);
-        emailET = (EditText) findViewById(R.id.loginNewAccountEmail);
-        pwdET = (EditText) findViewById(R.id.loginNewAccountSenha);
+        passwordET = (EditText) findViewById(R.id.loginNewAccountPassword);
+        confirmPasswordET = (EditText) findViewById(R.id.loginNewAccountConfirmPassword);
+        createAccountBT = (Button) findViewById(R.id.buttonloginNewAccount);
+        cancelBT = (Button) findViewById(R.id.buttonCancel);
+    }
 
-        Button logar = (Button) findViewById(R.id.buttonloginNewAccount);
-        Button cancelar = (Button) findViewById(R.id.btnCancelarTela2);
-
-
-        logar.setOnClickListener(new View.OnClickListener() {
+    private void setCreateAccountButtonListener(){
+        createAccountBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registerUser(v);
-                navigatetoLoginActivity(v);
-
+                if(isPasswordEquals()){
+                    //todo verificar se o nome de usuario esta disponivel no banco
+                    //todo criar o novo usuario no banco
+                    Intent createAccountIntent = new Intent(LoginNewAccount_2.this, LoginProfileChoose_4.class);
+                    startActivity(createAccountIntent);
+                } else {
+                    //todo avisar usuario da divergencia dos campos de password
+                    passwordET.setText("");
+                    confirmPasswordET.setText("");
+                }
             }
         });
-        cancelar.setOnClickListener(new View.OnClickListener() {
+    }
+    private void setCancelButtonListener(){
+        cancelBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            navigatetoLoginActivity(v);
+                Intent createAccountIntent = new Intent(LoginNewAccount_2.this, Login_1.class);
+                startActivity(createAccountIntent);
             }
         });
-
-
     }
-
-    public void registerUser(View view){
-        String name, email, password;
-        // Get NAme ET control value
-        name = nameET.getText().toString();
-        // Get Email ET control value
-        email = emailET.getText().toString();
-        // Get Password ET control value
-        password = pwdET.getText().toString();
-        // Instantiate Http Request Param Object
-//        RequestParams params = new RequestParams();
-        // When Name Edit View, Email Edit View and Password Edit View have values other than Null
-
-        // When any of the Edit View control left blank
-
-
-    }
-
-    /**
-     * Method that performs RESTful webservice invocations
-     *
-     * @param params
-     */
-   /* public void invokeWS(RequestParams params){
-        // Show Progress Dialog
-        prgDialog.show();
-        // Make RESTful webservice call using AsyncHttpClient object
-        AsyncHttpClient client = new AsyncHttpClient();
-
-        //TODO : Aqui utilizaremos  as CTE q o michel ira criar do cadastro de usuario
-        client.get("endereco do servico de cadastro",params ,new AsyncHttpResponseHandler() {
-            // When the response returned by REST has Http response code '200'
-            @Override
-            public void onSuccess(String response) {
-                // Hide Progress Dialog
-                prgDialog.hide();
-                try {
-                    // JSON Object
-                    JSONObject obj = new JSONObject(response);
-                    // When the JSON response has status boolean value assigned with true
-                    if(obj.getBoolean("status")){
-                        // Set Default Values for Edit View controls
-                        setDefaultValues();
-                        // Display successfully registered message using Toast
-                        Toast.makeText(getApplicationContext(), "You are successfully registered!", Toast.LENGTH_LONG).show();
-                    }
-                    // Else display error message
-                    else{
-                        errorMsgTV.setText(obj.getString("error_msg"));
-                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
-                    }
-                } catch (JSONException e) {
-                    // TODO Auto-generated catch block
-                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
-
-                }
-            }
-            // When the response returned by REST has Http response code other than '200'
-
-            public void onFailure(int statusCode, Throwable error,
-                                  String content) {
-                // Hide Progress Dialog
-                prgDialog.hide();
-                // When Http response code is '404'
-                if(statusCode == 404){
-                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code is '500'
-                else if(statusCode == 500){
-                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code other than 404, 500
-                else{
-                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }*/
-
-    /**
-     * Botao Cancelar
-     */
-    public void navigatetoLoginActivity(View view){
-        Intent loginIntent = new Intent(getApplicationContext(), Login_1.class);
-        // Clears History of Activity
-        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(loginIntent);
-    }
-
-    /**
-     * Set degault values for Edit View controls
-     */
-    public void setDefaultValues(){
-        nameET.setText("Sem nome");
-        emailET.setText("Sem email");
-        pwdET.setText("Sem senha");
+    private boolean isPasswordEquals(){
+        //verifica se os campos de password estao iguais
+        if(passwordET.getText().toString().equals(confirmPasswordET.getText().toString()))
+            return true;
+        else
+            return false;
     }
 }
