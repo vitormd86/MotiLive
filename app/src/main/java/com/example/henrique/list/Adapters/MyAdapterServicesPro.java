8c54878e9ca3b006cfb9d340cc9f7c42edec89e3
Vitor <vitormd86@gmail.com>
2015-06-09 16:01:48 -0300

    Update em pro_6 e pro_7

--- app/src/main/java/com/example/henrique/list/Adapters/MyAdapterServicesPro.java
+++ app/src/main/java/com/example/henrique/list/Adapters/MyAdapterServicesPro.java
@@ -17,12 +17,12 @@ import java.util.ArrayList;
 public class MyAdapterServicesPro extends ArrayAdapter<String>{
 
     public MyAdapterServicesPro(Context context, ArrayList<String> services) {
-        super(context, R.layout.view_professional_services, services);
+        super(context, R.layout.view_services_professional, services);
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater theInflator = LayoutInflater.from(getContext());
-        convertView = theInflator.inflate(R.layout.view_professional_services, parent, false);
+        convertView = theInflator.inflate(R.layout.view_services_professional, parent, false);
 
         TextView serviceName = (TextView) convertView.findViewById(R.id.serviceNameETPro_6);
         String service = getItem(position);
